1a6c77dfde7c12bec1e9b2824f0943228daea858
kk90 <kr.korycki@gmail.com>
2014-06-29 10:49:01 +0200

    initialcomit

---
+++ imageProc/ImageProcessor.js
@@ -0,0 +1,95 @@
+﻿
+var imageProc = ( function () {
+
+    var sqs, s3;
+    var processingImageData;
+    var BUCKETkk = 'kkpbucket';
+   
+  
+    function uploadConvertedFile(){
+
+        var fs = require('fs');
+        fs.readFile('./temp/temp'+processingImageData.Messages[0].Body.split( "|" )[0]+'.jpg', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        
+        var params = {
+        Bucket: BUCKETkk, // required
+        Key: (processingImageData.Messages[0].Body.split( "|" )[0]+"cv"), // required
+        Body: data,
+        };
+        s3.putObject(params, function(err, data) {
+             if (err) console.log(err, err.stack); // an error occurred
+             else     console.log(data);           // successful response
+            });
+});
+        }
+
+
+
+    function convert(){
+        //TODO
+        uploadConvertedFile();
+        }
+
+
+    function saveFile(ImageBody){
+        console.log(ImageBody);
+        var fs = require('fs');
+         fs.writeFile('./temp/temp'+processingImageData.Messages[0].Body.split( "|" )[0]+'.jpg',ImageBody, function(err) {
+             if(err) {
+        console.log(err);
+    } else {
+        console.log("The file was saved!");
+        convert();
+        }
+             });
+             
+        }
+    
+    
+    
+     function getImage( Id ) {
+        var params = {
+            Bucket: BUCKETkk, // required
+            Key: Id
+        };
+        s3.getObject( params, function ( err, data ) {
+            if ( err ) console.log( err, err.stack ); // an error occurred
+            else{
+                    saveFile(data.Body);
+                }        // successful response
+        });
+    }
+
+
+    
+
+
+    function process( imageData ) {
+        console.log( imageData.Messages[0].Body );
+
+        processingImageData = imageData;
+        
+        getImage( processingImageData.Messages[0].Body.split( "|" )[0] );
+
+
+    }
+
+    return {
+
+        init: function ( sqs_, s3_ ) {
+            console.log( "ImgProc INIT" );
+            sqs = sqs_;
+            s3 = s3_;
+        },
+
+        processImage: function ( imageData ) {
+            process( imageData )
+            }
+    }
+
+} () );
+
+exports.imageProc = imageProc
---
+++ imageProc/server.js
@@ -0,0 +1,58 @@
+﻿var AWS = require( "aws-sdk" );
+
+AWS.config.loadFromPath( 'config.json' );
+var s3 = new AWS.S3();
+
+AWS.config.loadFromPath( 'configSQS.json' );
+var sqs = new AWS.SQS();
+
+
+var QUEUEkk='https://sqs.us-west-2.amazonaws.com/983680736795/korycki';
+
+console.log( '_____INIT OK' );
+
+
+var imageProcessor=require( "./ImageProcessor" ).imageProc;
+
+imageProcessor.init(sqs,s3);
+
+
+
+
+function getMessage(){
+var params = {
+  QueueUrl: QUEUEkk,
+  MaxNumberOfMessages: 1,
+  VisibilityTimeout: 30,
+  WaitTimeSeconds: 10,
+
+  }
+
+    sqs.receiveMessage( params, function ( err, data ) {
+        if ( err ){
+            
+             //console.log("noMSG or ERR");
+             }
+         // an error occurred
+        else{ //console.log( data );  
+               imageProcessor.processImage(data);
+               var params = {
+                QueueUrl: QUEUEkk, // required
+                ReceiptHandle: data.Messages[0].ReceiptHandle  // required
+            };
+            sqs.deleteMessage( params, function ( err, data ) {
+                if ( err ) console.log( err, err.stack ); // an error occurred
+                else console.log( data );           // successful response
+                });
+            }         // successful response
+    });
+ }
+
+ getMessage();
+setInterval(function(){
+  getMessage();
+  }, 10*1000);
+
+
+
+
